8c9b8c18837f89fd3a78f57286758de9818c9a34
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-06 18:22:46 +0530

    Add EvenOdd task program

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        for(int i=2; i<10; i++) {
+            if(i%2!=0) {
+                System.out.println(i + " is odd");
+            } else {
+                System.out.println(i + " is even");
+            }
+        }
+    }
+}
