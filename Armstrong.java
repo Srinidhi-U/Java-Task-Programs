815b2535d529fe7b0091f2644aaef1527a115409
Srinidhi-U <srinidhiuppula4@gmail.com>
2026-09-06 18:22:50 +0530

    Add Armstrong task program

---
+++ Armstrong.java
@@ -0,0 +1,17 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int n=153;
+        int original=n;
+        int sum=0;
+        while(n>0) {
+            int digit=n%10;
+            sum=sum+(digit*digit*digit);
+            n=n/10;
+        }
+        if(sum==original) {
+            System.out.println(original + " is an Armstrong number");
+        } else {
+            System.out.println(original + " is not an Armstrong number");
+        }
+    }
+}
